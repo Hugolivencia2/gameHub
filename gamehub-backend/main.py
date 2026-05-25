28adb83cc53c30126c85147d69c1c2de21883093
JuanGarciaMaria <juan.garcia.maria99@gmail.com>
2026-05-25 10:57:10 +0200

    Add additional routers for noticias, multimedia, and ranking

--- gamehub-backend/main.py
+++ gamehub-backend/main.py
@@ -6,7 +6,9 @@ from fastapi.middleware.cors import CORSMiddleware
 # Importamos tu enrutador
 from routers import usuarios
 from routers import comentarios
-
+from routers import noticias
+from routers import multimedia
+from routers import ranking
 app = FastAPI(title="Game-Hub API")
 
 # Configuración CORS (Vital para que el frontend no dé errores de conexión)
@@ -21,7 +23,9 @@ app.add_middleware(
 # Conectamos el módulo de usuarios a la aplicación principal
 app.include_router(usuarios.router)
 app.include_router(comentarios.router)
-
+app.include_router(noticias.router)
+app.include_router(multimedia.router)
+app.include_router(ranking.router)
 @app.get("/")
 def ruta_raiz():
     return {"mensaje": "¡Servidor Backend de Game-Hub funcionando!"}
